d03eb0d9eaa0092d7d59a149d51763f7e3d135bb
Bit2hacker <134467823+Bit2hacker@users.noreply.github.com>
2023-09-22 09:11:15 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Kushalhk/Autofilter.git /Autofilter 
+  git clone https://github.com/Bit2hacker/insanebot/
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Autofilter 
